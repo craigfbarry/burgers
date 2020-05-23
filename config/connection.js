d7f72deaf528d82a331f7653780c70d5ca53b811
Craig Barry <craigfbarry@gmail.com>
2020-05-23 15:48:29 +0930

    Set up server and connection js installed node modules.

---
+++ config/connection.js
@@ -0,0 +1,19 @@
+
+//Dependencies
+const Sequelize = require("sequelize");
+
+
+// Creates mySQL connection using Sequelize
+const sequelize = new Sequelize("burgers_db","root","C@esar99",
+    {
+        host:       "localhost",
+        port:       3306,
+        dialect: "mysql",
+        pool: {
+          max: 5,
+          min: 0,
+          idle: 10000}
+    });
+
+//Export the connection
+module.exports = sequelize;
---
+++ server.js
@@ -0,0 +1,31 @@
+//Dependencies.
+const express = require("express");
+
+
+//Configure port to serve HTML.
+const PORT = process.env.PORT||8080
+
+//Set up Express middleware and static hosting.
+const app = express();
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Set up express handlebars.
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+
+//Set up the routes controller.
+const routes = require("./controllers/burgers_controller.js");
+app.use(routes);
+
+
+//Start server by listening on PORT.
+app.listen(PORT, function() {
+    // Log (server-side) when our server has started
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
+  
